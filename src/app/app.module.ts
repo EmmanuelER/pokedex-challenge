329887d7e56d159e001e41d54ab122c34799e751
EmmanuelER <emmanuel@neuronmexico.com>
2021-05-14 15:01:07 -0500

    update components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatMenuModule } from '@angular/material/menu';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { DetailsComponent } from './pages/details/details.component';
 @NgModule({
-  declarations: [AppComponent, NavbarComponent],
+  declarations: [AppComponent, NavbarComponent, ProfileComponent, DetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
